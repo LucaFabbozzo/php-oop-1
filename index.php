5ef6778b2887668ceaa7b7d518dec8776ec36f57
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-12-07 11:09:53 -0500

    corrections

--- index.php
+++ index.php
@@ -39,20 +39,11 @@ $movieList = [$movie1, $movie2];
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
   <title>php-oop-1</title>
 </head>
 
 <body>
-
-  <style> 
-    img {
-      width: 80px;
-    }
-  </style>
-
-
-
-
   <div class="container w-75 m-auto text-center">
     <table class="table w-75 m-auto my-5">
       <thead>

ea0cf10dce2e9224c3f37e776247fbdc5b6ac686
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-12-07 08:03:12 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!-- Oggi pomeriggio ripassate i primi concetti di classe, variabili e metodi d’istanza che abbiamo visto stamattina e create un file index.php in cui:
+- è definita una classe ‘Movie’
+=> all’interno della classe sono dichiarate delle variabili d’istanza
+=> all’interno della classe è definito un costruttore
+=> all’interno della classe è definito almeno un metodo
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+Bonus 1:
+Modificare la classe Movie in modo che accetti piú di un genere.
+Bonus 2:
+Creare un layout completo per stampare a schermo una lista di movies.
+Facciamo attenzione all’organizzazione del codice, suddividendolo in appositi file e cartelle. Possiamo ad esempio organizzare il codice
+- creando un file dedicato ai dati (tipo le array di oggetti) che potremmo chiamare db.php
+- mettendo ciascuna classe nel proprio file e magari raggruppare tutte le classi in una cartella dedicata che possiamo chiamare Models/
+- organizzando il layout dividendo la struttura ed i contenuti in file e parziali dedicati. -->
+
+<?php 
+
+
+echo 'Ciao';
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link rel="stylesheet" href="./css/style.css">
+  <title>php-oop-1</title>
+</head>
+
+<body>
+
+
+</body>
+
+</html>
